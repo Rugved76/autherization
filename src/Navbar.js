6cf24cba114f4cb8c88d84e0f515a630d5f8eff6
Rugved Wagh <89897353+Rugved76@users.noreply.github.com>
2023-06-18 12:50:24 +0530

    Add files via upload

---
+++ src/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ backgroundColor: "#22dd95", color: 'white  ' }}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/" style={{ color: 'white' }}>AUTH</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link " aria-current="page" href="/" style={{ color: 'white' }}>Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/Login" style={{ color: 'white' }}>Login</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/Signup" style={{ color: 'white' }}>SignUp</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
